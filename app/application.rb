4643da95acb275f75ba6740530f4fc22a2bdf1e7
Amy Cen <amy.cen@blackrock.com>
2019-06-02 06:51:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item = path.split("/items/").last
+      item = req.path.split("/items/").last
       if Item.all.include?(item)
         item.price
       else

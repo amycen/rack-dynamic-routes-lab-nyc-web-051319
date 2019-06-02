b3a725932258ea629ee54da2515ded891858dad3
Amy Cen <amy.cen@blackrock.com>
2019-06-02 07:00:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      binding.pry
+      #binding.pry
       found = Item.all.find {|i| i.name == item}
       if found
         resp.write found.price
